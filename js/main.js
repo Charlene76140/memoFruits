5b1dcb456727cbba89dba9ee97b4ae612923d53e
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-04-15 18:12:28 +0200

    random colors ok, dot counter ok, html and css ok

--- js/main.js
+++ js/main.js
@@ -1,41 +1,76 @@
 //-----------------------------------Variables---------------------------------------- 
+
 let containerCard = document.getElementById("containerCard");
 let gameCard = document.getElementsByClassName("gameCard");
 let chosenCard =[];
-let score = 0;
+let scoreHtml= document.querySelector('em');
+let win = 0;
+let score = 12;
 let COLORS = ["purple","grey","yellow","orange","purple","green","blue","red","green","yellow","blue","red","grey","orange"];
+let layer = document.getElementById("hiddenLayer");
 
 // ----------------------------------fonctions----------------------------------------
-//fonction pour créer les cartes du jeux
-function makeCard(){
-    let col = document.createElement("div");
-    col.classList.add("offset-2","col-3","offset-lg-1","col-lg-1","my-1","sizeCard","gameCard");
-    containerCard.appendChild(col);
-};
-
-
-// fonction pour mélanger les couleurs
-function MakeArrayShuffleColors(colors){
-    let random = colors.sort(()=> Math.random() - 0.5);
-    console.log(random);
-    return random;
-};
-
-//fonction pour parcourir le tableau des couleurs et crééer autant de carte que de couleurs
-function getColorCard(){
-    let newArrayColorsRandom = MakeArrayShuffleColors(COLORS);
-    for(color of newArrayColorsRandom){
+// fonction pour mélanger les couleurs et lancer la fonction makeCard
+function MakeShuffleColors(){
+    COLORS.sort(()=> Math.random() - 0.5);
+    console.log(COLORS);
+    for(color of COLORS){
         makeCard();
     }
 };
 
-getColorCard();
 
-//fonction pour remplacer la carte cliquez avec la couleur du tableau colors
+//fonction pour créer les cartes du jeux en fonction du nombre de couleur
+function makeCard(){
+    let createCol = document.createElement("div");
+    createCol.classList.add("offset-2","col-3","offset-lg-1","col-lg-1","my-1","sizeCard","gameCard");
+    containerCard.appendChild(createCol);
+};
+
+
+//fonction qui compare les deux cartes choisies
+function compareArray(){
+    if(chosenCard[0].style.backgroundColor === chosenCard[1].style.backgroundColor){
+        layer.classList.remove("layer"); 
+        win ++;
+        console.log(win);
+        
+    }
+    else{
+        chosenCard[0].style = "none";
+        chosenCard[1].style = "none";
+        layer.classList.remove("layer");
+        score --;
+        scoreHtml.textContent = score;
+    }
+    chosenCard = [];
+    scoreFinal();
+}
+
+// fonction de résultat final
+function scoreFinal(){
+    if(win === 7){
+        alert("c'est gagné");
+    }
+    
+    else if(score < 0){
+        alert("c'est perdu");
+    }
+};
+
+// -------------------------------------------------------------- My logic code ------------------------------------------------------------
+//affiche le nombre de tentatives restantes à l'utilisateur
+scoreHtml.textContent = score;
+
+//fonction permettant de générer des couleurs aléatoirement à partir du tableau de référence des couleurs
+MakeShuffleColors();
+
+
+
+//Boucle sur la classe "gameCard" et lance une fonction au click sur une des cartes
 for(let i = 0; i < gameCard.length; i++){
     gameCard[i].onclick = function(){
         chosenCard.push(gameCard[i]);
-        // chosenCard[0].style.pointerEvents = "none";
         gameCard[i].style.backgroundColor = COLORS[i];
 
         if(chosenCard.length === 2){
@@ -45,40 +80,5 @@ for(let i = 0; i < gameCard.length; i++){
         }
     };
 };
-
-//fonction qui compare les deux cartes choisies
-function compareArray(){
-    if(chosenCard[0].style.backgroundColor === chosenCard[1].style.backgroundColor){
-        let layer = document.getElementById("hiddenLayer");
-        layer.classList.remove("layer"); 
-        score ++;
-        console.log(score);
-    }
-
-    else{
-        let layer = document.getElementById("hiddenLayer");
-        layer.classList.remove("layer");
-        chosenCard[0].style = "none";
-        chosenCard[1].style = "none";
-    }
-    chosenCard = [];
-    console.log(chosenCard);
-    scoreFinal();
-}
-
-function scoreFinal(){
-    if(score === 7){
-        alert("bien joué! Vous avez gagné la partie");
-    }
-};
-
-
-// -------------------------------------------------------------- My logic code -----------------------------------------------------------------//
-
-
-
-
-
-
 
 
