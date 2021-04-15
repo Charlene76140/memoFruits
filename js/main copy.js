99bf4b5e4df64985a552010d4b4eb98f2cad6f4b
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-04-15 16:27:36 +0200

    add button game on home page + modify js code, for game play

--- js/main copy.js
+++ js/main copy.js
@@ -3,9 +3,7 @@ let containerCard = document.getElementById("containerCard");
 let gameCard = document.getElementsByClassName("gameCard");
 let chosenCard =[];
 let score = 0;
-
 let COLORS = ["purple","grey","yellow","orange","purple","green","blue","red","green","yellow","blue","red","grey","orange"];
-
 
 // ----------------------------------fonctions----------------------------------------
 //fonction pour créer les cartes du jeux
@@ -13,14 +11,23 @@ function makeCard(){
     let col = document.createElement("div");
     col.classList.add("offset-2","col-3","offset-lg-1","col-lg-1","my-1","sizeCard","gameCard");
     containerCard.appendChild(col);
-}
+};
+
+
+// fonction pour mélanger les couleurs
+function MakeArrayShuffleColors(colors){
+    let random = colors.sort(()=> Math.random() - 0.5);
+    console.log(random);
+    return random;
+};
 
 //fonction pour parcourir le tableau des couleurs et crééer autant de carte que de couleurs
 function getColorCard(){
-    for(color of COLORS){
+    let newArrayColorsRandom = MakeArrayShuffleColors(COLORS);
+    for(color of newArrayColorsRandom){
         makeCard();
     }
-}
+};
 
 getColorCard();
 
@@ -28,43 +35,45 @@ getColorCard();
 for(let i = 0; i < gameCard.length; i++){
     gameCard[i].onclick = function(){
         chosenCard.push(gameCard[i]);
-        // gameCard[i].classList.remove("bg-dark");
+        // chosenCard[0].style.pointerEvents = "none";
         gameCard[i].style.backgroundColor = COLORS[i];
-        
-        setTimeout(function(){
-            if(chosenCard.length === 2){
-                compareArray();}
-            }, 
-            
-        2500); 
-    }
+
+        if(chosenCard.length === 2){
+            let layer = document.getElementById("hiddenLayer");
+            layer.classList.add("layer"); 
+            setTimeout(function(){compareArray();}, 1500);   
+        }
+    };
 };
 
 //fonction qui compare les deux cartes choisies
 function compareArray(){
-    if(window.getComputedStyle(chosenCard[0],null).getPropertyValue("background") === window.getComputedStyle(chosenCard[1],null).getPropertyValue("background")){
-        console.log(chosenCard[0].style.background);
+    if(chosenCard[0].style.backgroundColor === chosenCard[1].style.backgroundColor){
+        let layer = document.getElementById("hiddenLayer");
+        layer.classList.remove("layer"); 
         score ++;
-        console.log(score)
+        console.log(score);
     }
 
     else{
-        // chosenCard[0].classList.add("test");
-        // chosenCard[1].classList.add("test");
+        let layer = document.getElementById("hiddenLayer");
+        layer.classList.remove("layer");
         chosenCard[0].style = "none";
         chosenCard[1].style = "none";
     }
     chosenCard = [];
+    console.log(chosenCard);
     scoreFinal();
 }
 
 function scoreFinal(){
     if(score === 7){
-        alert("bien joué! GG rené");
+        alert("bien joué! Vous avez gagné la partie");
     }
 };
 
 
+// -------------------------------------------------------------- My logic code -----------------------------------------------------------------//
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,36 +1,79 @@
-let cardTest = document.getElementsByClassName("gameCard");
-console.log(cardTest);
+//-----------------------------------Variables---------------------------------------- 
+let containerCard = document.getElementById("containerCard");
+let gameCard = document.getElementsByClassName("gameCard");
+let chosenCard =[];
+let score = 0;
+let COLORS = ["purple","grey","yellow","orange","purple","green","blue","red","green","yellow","blue","red","grey","orange"];
 
-let COLORS = ["purple","grey","yellow","black","purple","green","blue","red","green","yellow","blue","red","grey","black"];
-let chosenCard = [];
-
-// function getColorRandom(){
-//     let random = Math.floor(Math.random() * Math.floor(COLORS.length));
-//     return COLORS[random];
-// }
-// let choiceColor = getColorRandom();
-
-
-//function pour parcourir les cartes et afficher la carte choisie
-for (let i = 0; i < cardTest.length; i++){
-    console.log(i);
-    cardTest[i].onclick = function(){
-        chosenCard.push(COLORS[i]);
-        console.log(chosenCard);
-        cardTest[i].style.background = COLORS[i];
-
-        // for (let j = 0; j < chosenCard.length; j++){
-        //     if(chosenCard === COLORS[i]){
-        //         console.log("couleur présente");
-        //     }
-        //     else{
-        //         console.log("rien");
-        //     }
-            
-        // }
-    }
- 
+// ----------------------------------fonctions----------------------------------------
+//fonction pour créer les cartes du jeux
+function makeCard(){
+    let col = document.createElement("div");
+    col.classList.add("offset-2","col-3","offset-lg-1","col-lg-1","my-1","sizeCard","gameCard");
+    containerCard.appendChild(col);
 };
+
+
+// fonction pour mélanger les couleurs
+function MakeArrayShuffleColors(colors){
+    let random = colors.sort(()=> Math.random() - 0.5);
+    console.log(random);
+    return random;
+};
+
+//fonction pour parcourir le tableau des couleurs et crééer autant de carte que de couleurs
+function getColorCard(){
+    let newArrayColorsRandom = MakeArrayShuffleColors(COLORS);
+    for(color of newArrayColorsRandom){
+        makeCard();
+    }
+};
+
+getColorCard();
+
+//fonction pour remplacer la carte cliquez avec la couleur du tableau colors
+for(let i = 0; i < gameCard.length; i++){
+    gameCard[i].onclick = function(){
+        chosenCard.push(gameCard[i]);
+        // chosenCard[0].style.pointerEvents = "none";
+        gameCard[i].style.backgroundColor = COLORS[i];
+
+        if(chosenCard.length === 2){
+            let layer = document.getElementById("hiddenLayer");
+            layer.classList.add("layer"); 
+            setTimeout(function(){compareArray();}, 1500);   
+        }
+    };
+};
+
+//fonction qui compare les deux cartes choisies
+function compareArray(){
+    if(chosenCard[0].style.backgroundColor === chosenCard[1].style.backgroundColor){
+        let layer = document.getElementById("hiddenLayer");
+        layer.classList.remove("layer"); 
+        score ++;
+        console.log(score);
+    }
+
+    else{
+        let layer = document.getElementById("hiddenLayer");
+        layer.classList.remove("layer");
+        chosenCard[0].style = "none";
+        chosenCard[1].style = "none";
+    }
+    chosenCard = [];
+    console.log(chosenCard);
+    scoreFinal();
+}
+
+function scoreFinal(){
+    if(score === 7){
+        alert("bien joué! Vous avez gagné la partie");
+    }
+};
+
+
+// -------------------------------------------------------------- My logic code -----------------------------------------------------------------//
 
 
 
