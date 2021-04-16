292c5632245ca4521b6e9949d60065bf25481757
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-04-16 16:40:08 +0200

    add read.me + comment + modify js

---
+++ README.md
@@ -0,0 +1,54 @@
+# Site Memo Fruits
+
+Ce site est une représentation du célébre jeu du Mémory. Le joueur doit trouver toutes les paires de cartes pour gagner la partie.
+
+## Pourquoi ce projet?
+
+Ce projet a été réalisé dans le cadre de ma formation developpeur web et web mobile à l'AFPA afin d'évaluer mes compétences pour : 
+
+- Maquetter une application.
+- Développer une interface utilisateur web statique et adaptable.
+- Développer une interface utilisateur web dynamique.
+
+
+### Critères techniques de l'évaluation : 
+
+- utilisation de HTML5
+- utilisation de CSS3
+- utilisation de Framework Boostrap 5
+- utilisation d'une Base Boilerplate
+- utilisation de JavaScript avec respect des normes ES6
+- Produire des maquettes de type wireframe pour tous les écrans avec un design adapté
+- Produire une arborescence fonctionnelle de l’application reprenant les cas d’utilisation possible de la page (idéalement avec les spécifications fonctionnelles bonus)
+- Vos maquettes ont été réalisées avec un outil de maquettage
+- Vos wireframes sont accessibles dans un dossier DOC
+- Votre interface est responsive sur tous les supports
+- Votre HTML est validé par le W3C
+- Votre CSS est validé par le W3C
+- Votre HTML respecte les règles de sémantique
+- Votre CSS est organisé
+- Vous respectez le principe DRY
+- Votre code HTML, CSS, JS est commenté clair et lisible
+- Votre code est hébergé sur GitHub
+- Vous avez fait usage d’un logiciel de versionning
+- Votre site est hébergé via une GH-page
+- Le repository contient un readme, une description et des tags
+- Vous utilisez un outil de gestion de projet de type KANBAN. Idéalement pour chaque tâche vous estimez le temps et la priorité et vous vous mettez dans la peau de l’utilisateur (personne + verbe d’action).
+
+
+### Réalisé avec : 
+
+- HTML 5 et CSS3
+- Bootstrap
+- Javascript
+- API DOM JS
+- Visual studio Code
+- Git Bash
+- GitHub
+- Nano
+
+### Auteurs : 
+
+- Charlene76140
+
+
--- js/main.js
+++ js/main.js
@@ -1,34 +1,56 @@
 //-----------------------------------Variables---------------------------------------- 
-
-let containerCard = document.getElementById("containerCard");
 let gameCard = document.getElementsByClassName("gameCard");
 let chosenCard =[];
-let scoreHtml= document.querySelector('em');
+let remainingShotsHtml= document.querySelector('em');
 let win = 0;
-let score = 12;
+let remainingShots = 12;
 let COLORS = ["purple","grey","yellow","orange","purple","green","blue","red","green","yellow","blue","red","grey","orange"];
 let layer = document.getElementById("hiddenLayer");
 
-// ----------------------------------fonctions----------------------------------------
-// fonction pour mélanger les couleurs et lancer la fonction makeCard
-function MakeShuffleColors(){
-    COLORS.sort(()=> Math.random() - 0.5);
-    console.log(COLORS);
-    for(color of COLORS){
-        makeCard();
+// ----------------------------------My functions----------------------------------------
+//function to mix the colors and launch the makeCard function which creates as many cards as there are colors
+function makeShuffleColors(array){
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
+    return array;
 };
 
+//function which retrieves the array of mixed colors and calls the makeCard function to create as many cards as there are colors
+function makeNewGame(){
+    let colorShuffle = makeShuffleColors(COLORS);
+    console.log(colorShuffle);
+    for(color of colorShuffle){
+        makeCard();
+    };
+}
 
-//fonction pour créer les cartes du jeux en fonction du nombre de couleur
+//function to create the game cards on the html side
 function makeCard(){
     let createCol = document.createElement("div");
     createCol.classList.add("offset-2","col-3","offset-lg-1","col-lg-1","my-1","sizeCard","gameCard");
     containerCard.appendChild(createCol);
+    playGame();
 };
 
+//Function loop on the "gameCard" class and launch a function by clicking on a card
+function playGame(){
+    for(let i = 0; i < gameCard.length; i++){
+        gameCard[i].onclick = function(){
+            chosenCard.push(gameCard[i]);
+            gameCard[i].style.backgroundColor = COLORS[i];
 
-//fonction qui compare les deux cartes choisies
+            if(chosenCard.length === 2){
+                let layer = document.getElementById("hiddenLayer");
+                layer.classList.add("layer"); 
+                setTimeout(function(){compareArray();}, 1300);   
+            }
+        }
+    }
+};
+
+//function that compares the two chosen cards
 function compareArray(){
     if(chosenCard[0].style.backgroundColor === chosenCard[1].style.backgroundColor){
         layer.classList.remove("layer"); 
@@ -40,45 +62,27 @@ function compareArray(){
         chosenCard[0].style = "none";
         chosenCard[1].style = "none";
         layer.classList.remove("layer");
-        score --;
-        scoreHtml.textContent = score;
+        remainingShots --;
+        remainingShotsHtml.textContent = remainingShots;
     }
     chosenCard = [];
     scoreFinal();
 }
 
-// fonction de résultat final
+//function to define the end of the game according to the remaining moves or the cards found
 function scoreFinal(){
     if(win === 7){
-        alert("c'est gagné");
+        setTimeout(function(){alert("c'est gagné, Bien joué!");}, 800);
     }
     
-    else if(score <= 0){
-        alert("c'est perdu");
+    else if(remainingShots <= 0){
+        setTimeout(function(){alert("c'est perdu, dommage");}, 800);
     }
 };
 
 // -------------------------------------------------------------- My logic code ------------------------------------------------------------
-//affiche le nombre de tentatives restantes à l'utilisateur
-scoreHtml.textContent = score;
+//displays the number of remaining attempts to the user
+remainingShotsHtml.textContent = remainingShots;
 
-//fonction permettant de générer des couleurs aléatoirement à partir du tableau de référence des couleurs
-MakeShuffleColors();
-
-
-
-//Boucle sur la classe "gameCard" et lance une fonction au click sur une des cartes
-for(let i = 0; i < gameCard.length; i++){
-    gameCard[i].onclick = function(){
-        chosenCard.push(gameCard[i]);
-        gameCard[i].style.backgroundColor = COLORS[i];
-
-        if(chosenCard.length === 2){
-            let layer = document.getElementById("hiddenLayer");
-            layer.classList.add("layer"); 
-            setTimeout(function(){compareArray();}, 1500);   
-        }
-    };
-};
-
-
+// start game
+makeNewGame();
